d38465681d7587a2009c9172d8e210dc3a3a2063
Shirllie-Apiyo <apraise320@gmail.com>
2021-11-26 01:58:36 +0300

    defined the routes

--- Todo/src/app/app-routing.module.ts
+++ Todo/src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ToDoComponent } from './to-do/to-do.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'',redirectTo:'/todo', pathMatch:'full'},
+  {path:'todo',component:ToDoComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
