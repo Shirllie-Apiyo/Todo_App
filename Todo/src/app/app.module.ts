f57d99ed54b5e52969b6888bb75802f7dc671f20
Shirllie-Apiyo <apraise320@gmail.com>
2021-11-26 01:48:47 +0300

    generated a component called to-do

--- Todo/src/app/app.module.ts
+++ Todo/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ToDoComponent } from './to-do/to-do.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ToDoComponent
   ],
   imports: [
     BrowserModule,
